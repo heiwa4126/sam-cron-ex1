0adaa822a04a46a899fe253933ccd91378ff2bf8
heiwa4126 <heiwa4126@gmail.com>
2021-08-11 16:41:22 +0900

    double triggers example

--- hello_world/app.py
+++ hello_world/app.py
@@ -2,13 +2,22 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime, timezone
+import json
 
 
 def lambda_handler(event, context):
     """Sample pure Lambda function"""
 
     print(f"Hello! {str(datetime.now(tz=timezone.utc))}")
-    return
+    print(json.dumps(event))
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "event": event,
+            }
+        ),
+    }
 
 
 if __name__ == "__main__":
